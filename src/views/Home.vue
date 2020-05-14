49deb0b3762f4c9c31c4004d26140b8ee9423209
besoKing <alen.besenic@gmail.com>
2020-05-14 12:33:56 +0200

    commit on master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="container">
-    <v-card class="mx-auto" max-width="400" v-for="event in events" :key="event.id">
-      <v-img class="white--text align-end" height="200px" :src="event.picture_url">
-        <v-card-title>{{event.event_name}}</v-card-title>
-      </v-img>
+    <v-row>
+        <v-card class="mx-auto" max-width="400" v-for="event in events" :key="event.id">
+          <v-img class="white--text align-end" height="200px" :src="event.picture_url">
+            <v-card-title>{{event.event_name}}</v-card-title>
+          </v-img>
 
-      <v-card-subtitle class="pb-0">Date: {{event.date}}     Price:{{event.event_price}}</v-card-subtitle>
+          <v-card-subtitle class="pb-0">Date: {{event.date}}     Price:{{event.event_price}}</v-card-subtitle>
 
-      <v-card-text class="text--primary">
-        <div>{{event.location}}</div>
-        <div>{{event.description}}</div>
-      </v-card-text>
+          <v-card-text class="text--primary">
+            <div>{{event.location}}</div>
+            <div>{{event.description}}</div>
+          </v-card-text>
 
-      <v-card-actions>
-        <v-btn color="primary" @click="goToEventPage(event.event_url)">See Event</v-btn>
-      </v-card-actions>
-    </v-card>
+          <v-card-actions>
+            <v-btn color="primary" @click="goToEventPage(event.event_url)">See Event</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-row>
   </div>
 </template>
 
