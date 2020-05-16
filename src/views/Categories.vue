5a82735a42d640145f031630d60b14b2bedbee8c
unknown <bmacan@unipu.hr>
2020-05-16 13:37:33 +0200

    commit on master

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container fluid>
     <v-row>
-        <v-card class="mx-auto" max-width="400" v-for="cat in categories" :key="cat.title" outlined>
-            <v-img :src="cat.picture" router-link :to="cat.route"></v-img>
+        <v-card class="mx-auto" router-link :to="cat.route" max-width="400" v-for="cat in categories" :key="cat.title" outlined>
+            <v-img :src="cat.picture" ></v-img>
          <v-card-title>
              {{cat.title}}
          </v-card-title>
@@ -17,9 +17,9 @@ export default {
     data(){
         return{
             categories:[
-                {title:'Outdor', picture:'https://www.tramontana-outdoor.com/wp-content/uploads/2019/04/2-4.jpg', route:'/Nightlife'},
-                {title:'Nightlife', picture:'https://www.cvitanic.info/wp-content/uploads/2015/01/nightlife-7.jpg',  route:'@/Nightlife.vue'},
-                {title:'Library', picture:'https://internova.hr/wp-content/uploads/2019/03/internova_public_library_01.jpg', route:'/Libray'}
+                {title:'Outdor', picture:'https://www.tramontana-outdoor.com/wp-content/uploads/2019/04/2-4.jpg', route:'/Outdoor'},
+                {title:'Nightlife', picture:'https://www.cvitanic.info/wp-content/uploads/2015/01/nightlife-7.jpg',  route:'/Nightlife'},
+                {title:'Library', picture:'https://internova.hr/wp-content/uploads/2019/03/internova_public_library_01.jpg', route:'/Library'}
             ]
         }
     }
