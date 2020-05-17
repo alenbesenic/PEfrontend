194a399c0c15bb9c9e4969b21eb028eb13039da9
besoKing <alen.besenic@gmail.com>
2020-05-17 19:51:00 +0200

    commit on master

--- src/services/index.js
+++ src/services/index.js
@@ -96,6 +96,26 @@ let Events = {
         })
        return events;
     },
+    async getCanceledEvents(){
+        let response = await Service.get('/canceledEvents')
+        console.log(response.data)
+        let canceled = response.data.map(doc=>{
+            return{
+                id: doc.id,
+                date: doc.Date,
+                description: doc.Description,
+                event_name: doc.Event_Name,
+                event_price: doc.Event_Price,
+                event_url: doc.Event_URL,
+                category: doc.Category,
+                status: doc.Status,
+                picture_url: doc.PictureURL,
+                location: doc.Location
+            }
+        })
+        return canceled;
+       
+    },
     /*async getUpcomingEvents(){
         let response = await Service.get('/upcomingEvents')
         let upcomingEvents = response.data.map(doc=>{
