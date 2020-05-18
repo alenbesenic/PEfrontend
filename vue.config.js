6994a7489a3320e5323dbd15d3f831ef952f4233
unknown <bmacan@unipu.hr>
2020-05-18 20:26:11 +0200

    commit on master

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,15 @@ module.exports = {
     "vuetify"
   ]
 }
+const path = require('path');
+
+module.exports={
+  outputDir: path.resolve(__dirname, '../PE/public'),
+  devServer:{
+    proxy:{
+      '/PE':{
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+}
