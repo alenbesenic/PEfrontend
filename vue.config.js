f30f7438e01e19eca0a92b6d60a045631f14192f
unknown <bmacan@unipu.hr>
2020-05-19 19:39:05 +0200

    make it better

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,25 @@ module.exports = {
 }
 const path = require('path');
 
-module.exports={
-  outputDir: path.resolve(__dirname, '../PE/public'),
+module.exports = {
+  outputDir: path.resolve(__dirname, '../PE-master/public'),
   devServer:{
     proxy:{
-      '/src':{
+      '/events':{
+        target: 'http://localhost:3000'
+      },
+      '/category/nightlife':{
+        target: 'http://localhost:3000'
+      },
+      '/category/outdoor':{
+        target: 'http://localhost:3000'
+      },
+      '/category/library':{
+        target: 'http://localhost:3000'
+      },
+      '/canceledEvents':{
         target: 'http://localhost:3000'
       }
     }
-  }
+  },
 }
